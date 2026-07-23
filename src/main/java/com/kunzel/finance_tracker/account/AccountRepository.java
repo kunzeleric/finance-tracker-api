d165cf24d86c3219502d84d7559fa687f7c83b5d
Eric Kunzel <kunzel.eric@gmail.com>
2026-07-23 10:10:51 -0300

    feat: created `Account` repository and service files and new shared exception file

---
+++ src/main/java/com/kunzel/finance_tracker/account/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.kunzel.finance_tracker.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
---
+++ src/main/java/com/kunzel/finance_tracker/account/AccountService.java
@@ -0,0 +1,43 @@
+package com.kunzel.finance_tracker.account;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.kunzel.finance_tracker.shared.exceptions.NotFoundException;
+
+@Service
+public class AccountService {
+  private final AccountRepository accountRepository;
+
+  public AccountService(AccountRepository accountRepository) {
+    this.accountRepository = accountRepository;
+  }
+
+  public List<Account> getAllAccounts() {
+    return accountRepository.findAll();
+  }
+
+  public Account getAccountById(Long accountId) {
+    return accountRepository.findById(accountId).orElseThrow(() -> new NotFoundException(accountId, "CONTA"));
+  }
+
+  public Account createAccount(String name, BigDecimal balance, AccountType type) {
+    return accountRepository.save(Account.create(name, balance, type));
+  }
+
+  public Account updateAccount(Long accountId, String name, AccountType type) {
+    Account accountToUpdate = getAccountById(accountId);
+    accountToUpdate.updateDetails(name, type);
+    return accountToUpdate;
+  }
+
+  public void removeAccount(Long accountId) {
+    Account found = getAccountById(accountId);
+    accountRepository.delete(found);
+
+    // TODO: cascade delete de transações ligadas à conta
+  }
+
+}
