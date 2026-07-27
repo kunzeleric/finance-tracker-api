e2ed834f42d5df107d5c34287d8b87ad47294c34
Eric Kunzel <kunzel.eric@gmail.com>
2026-07-27 18:27:40 -0300

    chore(categories): including delete route in Controller

--- src/main/java/com/kunzel/finance_tracker/category/CategoryController.java
+++ src/main/java/com/kunzel/finance_tracker/category/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,5 +51,11 @@ public class CategoryController {
       @Valid @RequestBody UpdateCategoryRequest request) {
     Category updatedCategory = categoryService.updateCategory(categoryId, request.name(), request.type());
     return ResponseEntity.ok().body(new CategoryResponse(updatedCategory));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> removeCategory(@PathVariable("id") Long categoryId) {
+    categoryService.removeCategory(categoryId);
+    return ResponseEntity.noContent().build();
   }
 }
