aaa7dfc7f1029d6530f9539e39292eeaa4314daa
Eric Kunzel <kunzel.eric@gmail.com>
2026-07-29 15:53:25 -0300

    fix: added null check in update transaction request and returning type description in category response

--- src/main/java/com/kunzel/finance_tracker/category/dtos/CategoryResponse.java
+++ src/main/java/com/kunzel/finance_tracker/category/dtos/CategoryResponse.java
@@ -1,13 +1,12 @@
 package com.kunzel.finance_tracker.category.dtos;
 
 import com.kunzel.finance_tracker.category.Category;
-import com.kunzel.finance_tracker.category.CategoryType;
 
 public record CategoryResponse(Long categoryId, String name,
-    CategoryType type,
+    String type,
     Boolean isDefault) {
 
   public CategoryResponse(Category category) {
-    this(category.getId(), category.getName(), category.getType(), category.isDefault());
+    this(category.getId(), category.getName(), category.getType().getDescription(), category.isDefault());
   }
 }
--- src/main/java/com/kunzel/finance_tracker/transaction/dtos/UpdateTransactionRequest.java
+++ src/main/java/com/kunzel/finance_tracker/transaction/dtos/UpdateTransactionRequest.java
@@ -9,10 +9,10 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 
 public record UpdateTransactionRequest(
-                @Pattern(regexp = ".*\\S.*", message = "não pode estar em branco") String description,
-                @NotNull(message = "não pode estar em branco") @Positive(message = "não pode ser igual ou menor que 0 (zero)") BigDecimal amount,
-                @NotNull(message = "não pode estar em branco") LocalDate date,
-                @NotNull(message = "não pode estar em branco") Long accountId,
-                @NotNull(message = "não pode estar em branco") Long categoryId) {
+    @NotNull(message = "não pode estar em branco") @Pattern(regexp = ".*\\S.*", message = "não pode estar em branco") String description,
+    @NotNull(message = "não pode estar em branco") @Positive(message = "não pode ser igual ou menor que 0 (zero)") BigDecimal amount,
+    @NotNull(message = "não pode estar em branco") LocalDate date,
+    @NotNull(message = "não pode estar em branco") Long accountId,
+    @NotNull(message = "não pode estar em branco") Long categoryId) {
 
 }
